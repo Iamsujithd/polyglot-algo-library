686c0f49fa94986cbe931deeb11038388f96ae13
Iamsujithd <d.sujith@outlook.com>
2026-06-13 11:47:36 +0000

    feat: add iterative fibonacci implementation

--- python/math/fibonacci.py
+++ python/math/fibonacci.py
@@ -1,9 +1,8 @@
 def fib(n):
-  if n <= 1:
-    return n
-  a, b = 0, 1
-  for _ in range(2, n + 1):
-    a, b = b, a + b
-  return b
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
-print(fib(10))
+if __name__ == '__main__':
+    print(fib(10))
